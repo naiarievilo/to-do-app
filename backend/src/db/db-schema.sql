3c3edfd446b6a07197614c63936257d63de2ac2c
Ian Oliveira <naiarievilo@gmail.com>
2023-12-29 15:30:25 -0300

    Change userTodolists view db to LEFT JOIN
    Changed from implicit JOIN to LEFT JOIN due to edge case of list not
    having todos, which in turn would always return an empty result, even if
    the todo list existed.

--- backend/src/db/db-schema.sql
+++ backend/src/db/db-schema.sql
@@ -1,29 +1,31 @@
 CREATE TABLE IF NOT EXISTS users (
-       id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
-    email TEXT UNIQUE NOT NULL,
-     hash TEXT NOT NULL
+          id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, 
+       email TEXT UNIQUE NOT NULL,
+        hash TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS todolists (
-  user_id INTEGER REFERENCES users ON DELETE CASCADE,
-       id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-     date DATE NOT NULL
+          id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     user_id INTEGER NOT NULL REFERENCES users ON DELETE CASCADE,
+        date DATE NOT NULL,
+      UNIQUE (user_id, date)
 );
 
 CREATE TABLE IF NOT EXISTS todos (
-  list_id INTEGER REFERENCES todolists ON DELETE CASCADE,
-       id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-     todo TEXT,
-  checked BOOLEAN NOT NULL
+          id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     list_id INTEGER NOT NULL REFERENCES todolists ON DELETE CASCADE,
+        todo TEXT,
+     checked BOOLEAN NOT NULL
 );
 
 CREATE VIEW userTodoLists AS
-   SELECT u.id AS user_id,
-          tl.id AS list_id,
-          tl.date AS list_date,
-          td.id AS todo_id,
-          td.todo AS todo,
-          td.checked AS checked
-     FROM users AS u, todolists AS tl, todos AS td
-    WHERE u.id = tl.user_id AND tl.id = td.list_id;
+     SELECT tl.user_id,
+            tl.id AS list_id,
+            tl.date AS list_date,
+            td.id AS todo_id,
+            td.todo,
+            td.checked
+       FROM todolists AS tl
+  LEFT JOIN todos AS td
+         ON tl.id = td.list_id;
 
