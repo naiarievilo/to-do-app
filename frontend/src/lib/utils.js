6597e1c0e0abe0b0d4d387ee36033c9e30e621ce
Ian Oliveira <naiarievilo@gmail.com>
2023-12-30 19:09:59 -0300

    Add function to keep track of session

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,6 +1,20 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+import { redirect } from "react-router-dom";
+
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
+}
+
+export function loginRequired(route) {
+  const hasSession = localStorage.getItem("session");
+
+  if (!route.includes("home") && hasSession) {
+    return redirect("/home");
+  }
+
+  if (!hasSession) {
+    return redirect("/login");
+  }
 }
